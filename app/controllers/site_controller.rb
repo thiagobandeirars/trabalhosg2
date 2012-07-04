6920491edbbf33ea44a3719fd2ae5e212d63e555
Thiago Dorneles <thiago@mint>
2012-07-04 00:58:06 -0300

    HTML

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -9,7 +9,8 @@ class SiteController < ApplicationController
   def pesquisa
     @categorialivros = Categorialivro.all   
     q= "%#{params[:pesquisa]}%"
-    @livros = Livro.where("titulo like ?",q)
+    #@livros = Livro.where("titulo like ?",q)
+    @livros = Livro.find(:all, :conditions => ['titulo like ?', q])
   end
 
   def categorias
@@ -31,29 +32,31 @@ class SiteController < ApplicationController
   end
 
   def locacao
+    @categorialivros = Categorialivro.all
     @locacao = Locacao.new
 
     if params[:id]
-
       @livro = Livro.find(params[:id])
 
-      Locacao.create(:usuario_id => @current_user.id,
-                     :livro_id   => @livro.id,
-                     :data_locacao => Date.today,
-                     :data_prevista_dev => Date.today + @livro.prazo_entrega,
-                     :data_devolucao => nil,
-                     :multa => nil)
+      @locacao.usuario_id = @current_user.id
+      @locacao.livro_id = @livro.id
+      @locacao.data_locacao = Date.today
+      @locacao.data_prevista_dev = Date.today + @livro.prazo_entrega
+      @locacao.data_devolucao = nil
+      @locacao.multa = 0
 
-      respond_to do |format|
-        if @locacao.save
-          flash[:notice] = "Livro locado com sucesso"
-        end
+      if @locacao.save
+	flash[:notice] = "Locação realizada com sucesso!"
+        #format.html { redirect_to @locacao, :notice => 'Locacao was successfully created.' }
+        #format.json { render :json => @locacao, :status => :created, :location => @locacao }
+      else
+	flash[:error] = "Não foi possível realizar locação!"
+        #format.html { render :action => "new" }
+        #format.json { render :json => @locacao.errors, :status => :unprocessable_entity }
       end
-
     else
       flash[:error] = "Livro indicado nao existe"
     end
-      
   end
   
 end
